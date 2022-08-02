70dc659f6f389617149f5772585cace6253391a1
rishavkrm <rishavkumar2573@gmail.com>
2022-08-02 17:35:00 +0530

    removed httpOnly:true

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,8 +17,8 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true
+    )
+    // ,httpOnly: true
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
@@ -61,8 +61,8 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
+    expires: new Date(Date.now() + 10 * 1000)
+    // ,httpOnly: true
   });
   res.status(200).json(res.redirect('/user/login'));
 };
